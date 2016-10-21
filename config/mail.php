94ef63213ebbcf02b34208b81c0678881471a863
Unknown <jonatancastro1@gmail.com>
2016-10-20 19:05:05 -0500

    Arreglos para email

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'micolegiofinlandes@gmail.com',
+        'name' => 'Colegio Finlandes',
     ],
 
     /*
