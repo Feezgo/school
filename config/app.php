d1039a785cc88bf6355ad32795d185e8df690c53
Unknown <jonatancastro1@gmail.com>
2016-10-25 20:21:04 -0500

    Adelanto pdf

--- config/app.php
+++ config/app.php
@@ -166,8 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
-        //
+        Vsmoraes\Pdf\PdfServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,7 +224,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Carbon' => Carbon\Carbon::class
+        'Carbon' => Carbon\Carbon::class,
+        'PDF' => Vsmoraes\Pdf\PdfFacade::class,
     ],
 
 ];
