eb087df46edb126db649ca0dea19e4227ebbc6e0
Unknown <jonatancastro1@gmail.com>
2016-10-27 22:52:53 -0500

    Impresion de factura funcionando

--- resources/views/pagos/factura.blade.php
+++ resources/views/pagos/factura.blade.php
@@ -3,25 +3,42 @@
 <head>
 	<meta charset="UTF-8">
 	<title></title>
+	<style>
+		@page {
+			margin-top: 10px;
+			margin-bottom: 0px;
+			margin-left: 10px;
+			margin-right: 10px;
+		}
+	</style>
 </head>
 <body>
-	Test<br>
-	Test1<br>
-	Test2<br>
-	Test3<br>
-	Test4<br>
-	Test5<br>
-	Test6<br>
-	Test7<br>
-	Test8<br>
-	Test8<br>
-	Test8<br>
-	Test8<br>
-	Test8<br>
-	Test8<br>
-	Test8<br>
-	Test8<br>
-	Test8<br>
-	Test8<br>
+	<h5 style="text-align: center;">
+		<img style="max-width:50px; margin-top: 1px;" src="{{ public_path('/imagen/cole.jpg') }}"> <br><br>
+		COLEGIO FINLANDES<br>
+		Juan Pablo II
+	</h5>
+	<?php
+		$factura = -1;
+	?>
+
+	<table border-collapse="collapse" border="none" width="100%">
+	<tr style="background-color:#aaa">
+		<th width="65%"><small>Pago</small></th><th align="center"><small>$</small></th>
+	</tr>
+	@foreach($planes_de_pagos as $plan)
+		<?php 
+			if ($factura != $plan->factura)
+			{
+				$factura = $plan->factura;
+				echo '<tr style="background-color:#ddd"><td colspan="2"><small>Fact. '.date('Ymd').$plan->factura.'</td></small></tr>';
+			}
+		 ?>
+		 <tr>
+		 	<td><small>{{ $plan->pago['descripcion'] }}</small></td><td align="right"><small>${{ number_format($plan->pagado, 0) }}</small></td>
+		 </tr>
+		 <br>
+	@endforeach
+	</table>
 </body>
 </html>
